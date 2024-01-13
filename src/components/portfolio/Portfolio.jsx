8289ce99797f70dfd44892f1b82433df0c5ef2e3
danielkebede381@gmail.com <danielkebede381@gmail.com>
2024-01-13 08:16:38 -0500

    github link configured

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import IMG1 from '../../assets/portfolio1.png';
-import IMG2 from '../../assets/portfolio2.png';
+import IMG2 from '../../assets/admindashboard.png';
 import IMG3 from '../../assets/portfolio14.png';
-import IMG4 from '../../assets/portfolio10.png';
+import IMG4 from '../../assets/facialexpression.png';
 import IMG5 from '../../assets/portfolio7.png';
 import IMG6 from '../../assets/portfolio6.png';
 import './Portfolio.css'
@@ -15,44 +15,44 @@ const data=[
   {
     id:1,
     image:IMG1,
-    title:'Ebook tutorial App',
-    github:'https://gitHUb.com/danielkeb/ebook',
-    demo:'https://dribble.com/shots/23424'
+    title:'Simple Calculator',
+    github:'https://gitHUb.com/danielkeb/tutorial-App-/calcApp/calculator',
+    
   },
   {
     id:2,
     image:IMG2,
-    title:'Admin Dashboard for GET-PC Manage System',
-    github:'https://github.com/danielkeb/pc_register-system',
-    demo:'https://www.figma.com/proto/GYdsF4hKijpHv4F9NnrwNl/HITMS?type=design&node-id=1-2&t=NPlbAWDC6gk9Xo1N-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A2'
+    title:'GETPCMS Intern Project',
+    github:'https://github.com/danielkeb/Intern_project',
+    
   },
   {
     id:3,
     image:IMG3,
     title:'To do app food Delivery',
-    github:'https://github.com/danielkeb/pc_register-system',
-    demo:'https://dribble.com/shots/23424'
+    github:'https://github.com/danielkeb/flutter-app',
+   
   },
   {
     id:4,
     image:IMG4,
-    title:'Hospital Card Reservation system',
-    github:'https://github.com/danielkeb/pc_register-system',
-    demo:'https://dribble.com/shots/23424'
+    title:'Music Reccomendation based on Facial expressions',
+    github:'https://github.com/danielkeb/emoton-based-',
+   
   },
   {
     id:5,
     image:IMG5,
-    title:'GET PC-Management System',
-    github:'https://github.com/danielkeb/pc_register-system',
-    demo:'https://www.figma.com/proto/GYdsF4hKijpHv4F9NnrwNl/HITMS?type=design&node-id=1-2&t=NPlbAWDC6gk9Xo1N-0&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A2'
+    title:'AGSMS as Saas',
+    github:'https://github.com/danielkeb/Abune_Gorgoriwos_school_maangement_system',
+    
   },
   {
     id:6,
     image:IMG6,
     title:'E-learning system ',
-    github:'https://gitHUb.com/danielkeb/flutter-app',
-    demo:'https://dribble.com/shots/23424'
+    github:'https://gitHUb.com/danielkeb/flutt',
+    
   },
 
 ]
@@ -62,7 +62,7 @@ const Portfolio = () => {
     <h5 >what  Recent Work   </h5>
     <h2>Portfolio</h2>
     <div className="container portfolio_container">{
-   data.map(({id,image,title,github,demo})=>{
+   data.map(({id,image,title,github})=>{
     return(
       <article  key={id} className="portfolio_items">
       <div className='portfolio_item-image'>
@@ -71,7 +71,7 @@ const Portfolio = () => {
       <h3>{title}</h3>
       <div className='portfolio_item-cta'>
       <a href={github} className='btn' target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faGithub}/></a>
-      <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+      
 
       </div>
     </article>
